c3091890f865a091dd8100cfa0aac0065292eb60
zio <zio4272@naver.com>
2017-12-05 12:06:39 +0900

    전화알림 팝업 + 종료시 팝업종료 완료 / 데이터 넣는중 (전화번호완료)

--- app/src/main/java/kr/co/cgb/academycommunity/CallRecevierActivity.java
+++ app/src/main/java/kr/co/cgb/academycommunity/CallRecevierActivity.java
@@ -7,17 +7,20 @@ import kr.co.cgb.academycommunity.data.User;
 
 public class CallRecevierActivity extends BaseActivity {
 
+    public static CallRecevierActivity act = null;
 
     private android.widget.TextView nameTxt;
     private android.widget.TextView phoneNumTxt;
     String receivePhoneNum;
     String receiveName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_recevier);
         receivePhoneNum = (String) getIntent().getSerializableExtra("phonenum");
         receiveName = (String) getIntent().getSerializableExtra("name");
+        act = this;
         bindViews();
         setupEvents();
         setValues();
--- app/src/main/java/kr/co/cgb/academycommunity/util/PhoneStateReceiver.java
+++ app/src/main/java/kr/co/cgb/academycommunity/util/PhoneStateReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.text.Layout;
 import android.view.LayoutInflater;
@@ -28,7 +29,7 @@ import kr.co.cgb.academycommunity.data.User;
 
 public class PhoneStateReceiver extends BroadcastReceiver {
 
-    User user;
+
     @Override
     public void onReceive(final Context context, Intent intent) {
 
@@ -36,18 +37,18 @@ public class PhoneStateReceiver extends BroadcastReceiver {
             System.out.println("Receiver start");
             String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            User user = (User) intent.getSerializableExtra("user");
+//            User user = (User) intent.getSerializableExtra("user");
 
             if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                 Toast.makeText(context,"Incoming Call State",Toast.LENGTH_SHORT).show();
                 Toast.makeText(context,"Ringing State Number is -"+incomingNumber,Toast.LENGTH_SHORT).show();
-                if (incomingNumber.equals(user.getUserPhoneNum())) {
-                    Toast.makeText(context, "전화한사람 이름" + user.getUserName(), Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(context, "모르는 번호 입니다.", Toast.LENGTH_SHORT).show();
-                }
-                
+//                if (incomingNumber.equals(user.getUserPhoneNum())) {
+//                    Toast.makeText(context, "전화한사람 이름" + user.getUserName(), Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//                    Toast.makeText(context, "모르는 번호 입니다.", Toast.LENGTH_SHORT).show();
+//                }
+//
 //                LayoutInflater inflater = LayoutInflater.from(context);
 //                View alertLayout = inflater.inflate(R.layout.call_popup_top,null);
 //                 TextView tv_call_number = (TextView) alertLayout.findViewById(R.id.tv_call_number);
@@ -64,7 +65,7 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 
                 Intent mIntent = new Intent(context.getApplicationContext(), CallRecevierActivity.class);
                 mIntent.putExtra("phonenum", incomingNumber);
-                mIntent.putExtra("name", user.getUserName());
+//                mIntent.putExtra("name", user.getUserName());
                 mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(mIntent);
 
@@ -75,6 +76,7 @@ public class PhoneStateReceiver extends BroadcastReceiver {
             }
             if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
                 Toast.makeText(context, "Call Idle State", Toast.LENGTH_SHORT).show();
+                CallRecevierActivity.act.finish();
             }
         } catch (Exception e) {
             e.printStackTrace();
